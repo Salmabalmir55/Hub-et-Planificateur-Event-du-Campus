172f85c0d850949f5f0214b15ce389a28b6f08ac
Houda <houda.riyad-etu@etu.univh2c.ma>
2026-05-30 01:08:17 +0100

    Commit 1 : Ajout des Exceptions < GlobalExceptionHandler>

---
+++ src/main/java/net/planificateureventcampus/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package net.planificateureventcampus.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("success", false);
+        response.put("message", "Erreur de validation");
+        response.put("errors", errors);
+        response.put("status", 400);
+
+        return ResponseEntity.badRequest().body(response);
+    }
+}
