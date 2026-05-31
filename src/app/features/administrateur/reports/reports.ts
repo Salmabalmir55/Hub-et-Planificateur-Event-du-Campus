e0c6295bf32121f45242779fec08b905e15e26c9
Sana <sana.timourti-etu@etu.univh2c.ma>
2026-05-31 22:40:24 +0100

    commit 6:ajout des fichiers Report pour administrateur

--- src/app/features/administrateur/reports/reports.ts
+++ src/app/features/administrateur/reports/reports.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-reports',
+  standalone: true, // <-- L'ajout indispensable pour que le routage fonctionne
   imports: [],
   templateUrl: './reports.html',
   styleUrl: './reports.scss',
